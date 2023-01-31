28125b5e2ae985152046950c414cf8ddb365e242
shohan-pherones <shohan.sub.56@gmail.com>
2023-01-31 23:23:44 +0600

    first commit

---
+++ 463-island-perimeter.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function (grid) {
+  let perimeter = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        perimeter += 4;
+
+        if (i > 0 && grid[i - 1][j] === 1) perimeter -= 2;
+        if (j > 0 && grid[i][j - 1] === 1) perimeter -= 2;
+      }
+    }
+  }
+
+  return perimeter;
+};
+
+console.log(
+  islandPerimeter([
+    [0, 1, 0, 0],
+    [1, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0],
+  ])
+);
